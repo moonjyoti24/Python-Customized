8311bebe838e5c2d555d0780f2ad197dbf52136f
moonjyoti24 <moonjyoti7@gmail.com>
2026-02-04 14:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello():
     return "Welcome to Moonjyoti's 1st Python Web Program to get the Square of any number you input!"
 
+# Take the number as input    
+number = int(input('Enter a number to get its square '))
+ 
+# Define a function to calculate Square
+def calculateSquare(num):
+    return num*num
+ 
+# print the output
+print("Square of ",number, "is ", calculateSquare(number))
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
