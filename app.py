3e66d033938f32e572c4511e19179efc7ad1f04b
moonjyoti24 <moonjyoti7@gmail.com>
2026-02-04 16:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
-@app.route('/', '/square', methods=['GET'])
-
+@app.route('/')
 def hello():
-    return "Welcome to Moonjyoti's Webpage!"
-    
-def squarenumber():
-    num = request.args.get('num')
-
-    if num is None:  # No number entered, show input form
-        return render_template('squarenum.html')
-    elif num.strip() == '':  # Empty input
-        return "<h1>Invalid number. Please enter a number.</h1>"
-    try:
-        square = int(num) ** 2
-        return render_template('answer.html', squareofnum=square, num=num)
-    except ValueError:
-        return "<h1>Invalid input. Please enter a valid number.</h1>"
+    return "Welcome to Moonjyoti's Webpage. Wish you a happy new year!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
