4837cd245d1266a14f594270f9076d7baae35cba
moonjyoti24 <moonjyoti7@gmail.com>
2026-03-31 11:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to Moonjyoti's Webpage. If you see this message, then you have deployed Containerized apps Successfully!"
+    return "Welcome to Moonjyoti's Webpage. If you see this message, then I have deployed Containerized apps Successfully. YAY...!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
