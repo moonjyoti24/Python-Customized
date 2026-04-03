6e5e4570bf859ba3d978e0905e6e8f269c259cb5
moonjyoti24 <moonjyoti7@gmail.com>
2026-04-03 10:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to Suchithra's Webpage. Thank you for visiting this site !"
+    return "Welcome to Moonjyoti's Webpage. Thank you for visiting this site !"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
